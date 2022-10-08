6deef4efba4a3b01b9d8f02c823d776afb4c55fc
ansamAY <ansam.yousry@gmail.com>
2022-10-08 07:15:29 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -2,13 +2,13 @@ from hello import toyou, add, subtract
 
 
 def setup_function(function):
-    print("Running Setup: %s" % function.__name__)
-    function.x = 10
+	print("Running Setup: %s" % function.__name__)
+	function.x = 10
 
 
 def teardown_function(function):
-    print("Running Teardown: %s" % function.__name__)
-    del function.x
+	print("Running Teardown: %s" % function.__name__)
+	del function.x
 
 def test_hello_subtract():
-    assert subtract(test_hello_subtract.x) == 9
+	assert subtract(test_hello_subtract.x) == 9

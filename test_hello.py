343ab1779835280a233cc62268bbfadc5b0ee8dc
ansamAY <ansam.yousry@gmail.com>
2022-10-08 07:12:20 +0200

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -2,18 +2,13 @@ from hello import toyou, add, subtract
 
 
 def setup_function(function):
-	print("Running Setup: %s" % function.__name__)
-	function.x = 10
+    print("Running Setup: %s" % function.__name__)
+    function.x = 10
 
 
 def teardown_function(function):
-	print("Running Teardown: %s" % function.__name__)
-	del function.x
-
-
-### Run to see failed test
-#def test_hello_add():
-#    assert add(test_hello_add.x) == 12
+    print("Running Teardown: %s" % function.__name__)
+    del function.x
 
 def test_hello_subtract():
-	assert subtract(test_hello_subtract.x) == 9
+    assert subtract(test_hello_subtract.x) == 9

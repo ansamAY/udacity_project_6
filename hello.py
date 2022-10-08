511bbbe05d2fc6ef7ae033ed0a1ce8532c4987a0
ansamAY <ansam.yousry@gmail.com>
2022-10-08 05:39:29 +0000

    some changes

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
 def toyou(x):
-<<<<<<< HEAD
     return "hi %s" % x
-=======
-	return "hi %s" % x
->>>>>>> 17f6648fab6521ad2df6f410ea249da4f0074799
 
 
 def add(x):
@@ -11,8 +7,4 @@ def add(x):
 
 
 def subtract(x):
-<<<<<<< HEAD
     return x - 1
-=======
-	return x - 1
->>>>>>> 17f6648fab6521ad2df6f410ea249da4f0074799
--- test_hello.py
+++ test_hello.py
@@ -1,19 +1,19 @@
 from hello import toyou, add, subtract
 
+
 def setup_function(function):
     print("Running Setup: %s" % function.__name__)
     function.x = 10
 
 
 def teardown_function(function):
-<<<<<<< HEAD
     print("Running Teardown: %s" % function.__name__)
     del function.x
 
-=======
-	print("Running Teardown: %s" % function.__name__)
-	del function.x
->>>>>>> 17f6648fab6521ad2df6f410ea249da4f0074799
+
+### Run to see failed test
+#def test_hello_add():
+#    assert add(test_hello_add.x) == 12
 
 def test_hello_subtract():
     assert subtract(test_hello_subtract.x) == 9
